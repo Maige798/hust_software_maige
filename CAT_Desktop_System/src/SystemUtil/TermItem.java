4b2f9ff1c31ee175977e6486492c6bf197d0ac25
Maige798 <3317991292@qq.com>
2023-05-13 14:45:09 +0800

    Maige-提交代码

---
+++ CAT_Desktop_System/src/SystemUtil/TermItem.java
@@ -0,0 +1,50 @@
+package SystemUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TermItem {
+    String title;
+    List<Sentence> termList;
+
+    // 构造方法
+    public TermItem(String Title) {
+        this.title = Title;
+        this.termList = new ArrayList<>();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder content = new StringBuilder("@TermItem{\r\n" + "\t<title> " + this.title);
+        for (Sentence listItem : termList)
+            content.append("\t<term> ").append(listItem.toString()).append("\r\n");
+        content.append("}\r\n");
+        return content.toString();
+    }
+
+    // 当一个新条目使用语言未出现在termList中时，在termList中添加此新条目
+    public void AddTerm(Sentence sentence){
+        for(Sentence listItem:termList) {
+            if (listItem.language.EqualsTo(sentence.language)) {
+                System.err.println("Language:" + sentence.language.toString() + " already exists in TermItem:" + this.title);
+                return;
+            }
+        }
+        termList.add(sentence);
+    }
+
+    // 根据语言类型移除特定的条目
+    public void RemoveTerm(Language language){
+        termList.removeIf(listItem -> listItem.language.EqualsTo(language));
+    }
+
+    // 获取目标语言类型的对应条目
+    public Sentence GetTerm(Language language) {
+        for (Sentence listItem : termList) {
+            if (listItem.language.EqualsTo(language))
+                return listItem;
+        }
+        System.err.println("Language:" + language + " doesn't exist in TermItem:" + this.title);
+        return null;
+    }
+}
---
+++ CAT_Desktop_System/src/SystemUtil/TranslationItem.java
@@ -0,0 +1,52 @@
+package SystemUtil;
+
+public class TranslationItem {
+    Sentence origin;
+    Sentence translation;
+
+    @Override
+    public String toString() {
+        StringBuffer buffer = new StringBuffer();
+        buffer.append("@TranItem{\r\n").append("\t<ori> ").append(origin).append("\r\n");
+        if (this.translation != null)
+            buffer.append("\t<tran> ").append(translation).append("\r\n");
+        else
+            buffer.append("\t<blank>\r\n");
+        buffer.append("}\r\n");
+        return buffer.toString();
+    }
+
+    // 构造方法
+    public TranslationItem() {
+        this.origin = new Sentence();
+        this.translation = new Sentence();
+    }
+
+    public TranslationItem(Sentence origin, Sentence translation) {
+        this.origin = origin;
+        this.translation = translation;
+    }
+
+    public TranslationItem(Language originLanguage, String origin) {
+        this.origin = new Sentence(originLanguage, origin);
+        this.translation = null;
+    }
+
+    // 设置原文
+    public void SetOrigin(Sentence Origin) {
+        this.origin = Origin;
+    }
+
+    public void SetOrigin(Language language, String text) {
+        this.origin = new Sentence(language, text);
+    }
+
+    // 设置译文内容
+    public void SetTranslation(Sentence Translation) {
+        this.translation = Translation;
+    }
+
+    public void SetTranslation(Language language, String text) {
+        this.translation = new Sentence(language, text);
+    }
+}
