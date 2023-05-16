5954f58fa1fa0a59b6a78d7e781f01596aa6c102
sing-ban <3073702249@qq.com>
2023-05-16 15:01:21 +0800

    Singban-提交代码

--- CAT_Desktop_System/src/javaswing/ProjectInterface.java
+++ CAT_Desktop_System/src/javaswing/ProjectInterface.java
@@ -8,15 +8,15 @@ public class ProjectInterface {
     Button a=new Button("新建项目");
     Button b=new Button("打开");
     JTextField c=new JTextField("欢迎使用");
-    JList jList = new JList();
-		jList.addListSelectionListener(new void ListSelectionListener(){
-        @Override
-        public void valueChanged(ListSelectionEvent e) {
-            if(!jList.getValueIsAdjusting()){	//设置只有释放鼠标时才触发
-                System.out.println(jList.getSelectedValue());
-            }
-        }
-    });
+//    JList jList = new JList();
+//		jList.addListSelectionListener(new void ListSelectionListener(){
+//        @Override
+//        public void valueChanged(ListSelectionEvent e) {
+//            if(!jList.getValueIsAdjusting()){	//设置只有释放鼠标时才触发
+//                System.out.println(jList.getSelectedValue());
+//            }
+//        }
+//    });
 
 
 
