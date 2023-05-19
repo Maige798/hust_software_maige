6757bf3582616ce9ada24c4d3553ae5228135862
Maige798 <3317991292@qq.com>
2023-05-19 16:26:25 +0800

    Maige-提交代码

--- CAT_Desktop_System/src/ProjectSystem/ProjectManager.java
+++ CAT_Desktop_System/src/ProjectSystem/ProjectManager.java
@@ -6,7 +6,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ProjectManager {
-    public List<CAT_Project> projectList = new ArrayList<>();
+    public List<CAT_Project> projectList = new ArrayList<>(); // 项目列表
+    public CAT_Project currentProject; // 当前打开的项目
+    public TranslationFile currentTranslationFile; // 当前正处于编辑状态的文件
+
     // 实现单例模式
     public static ProjectManager instance = new ProjectManager();
 
@@ -14,13 +17,22 @@ public class ProjectManager {
 
     }
 
-    public CAT_Project CreateProject(String name) {
+    // 新建一个项目
+    public CAT_Project CreateProject(String name,String save) {
+        CAT_Project project = new CAT_Project(name, save);
+        instance.projectList.add(project);
+        return project;
+    }
+
+    // 根据路径读取一个项目文件，并添加至projectList中
+    public CAT_Project ReadProject(String save){
         // todo
         return null;
     }
 
-    public void LoadProject(CAT_Project project) {
+    public CAT_Project LoadProject () {
         // todo
+        return null;
     }
 
     public void SaveProject(CAT_Project project) {
