5d540aa95fba184e5eadd2c188a57637fec1f8f0
lcwzy <273896827@qq.com>
2023-05-26 20:34:10 +0800

    lcwzy-提交代码 （采用测试驱动开发，重构术语库文件读取功能）

--- CAT_Desktop_System/src/javaswing/MemoryLibraryWindow.java
+++ CAT_Desktop_System/src/javaswing/MemoryLibraryWindow.java
@@ -5,12 +5,12 @@ import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 import java.awt.*;
 
-public class TermLibraryInterface extends JFrame{
+public class MemoryLibraryWindow extends JFrame {
     public static void main(String[] args){
-        TermLibraryInterface windows = new TermLibraryInterface();
+        MemoryLibraryWindow windows = new MemoryLibraryWindow();
         windows.setVisible(true);
     }
-    public TermLibraryInterface()
+    public MemoryLibraryWindow()
     {
         init();
     }
@@ -19,7 +19,7 @@ public class TermLibraryInterface extends JFrame{
     {
         //窗口设置
         setSize(1000,650);
-        setTitle("术语库");
+        setTitle("翻译记忆库");
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setBackground(Color.decode("#EAECF2"));
         setResizable(false);
@@ -27,14 +27,14 @@ public class TermLibraryInterface extends JFrame{
         //创建顶部菜单栏
         JMenuBar menuBar=createJMenuBar();
 
-        //创建术语库列表
-        JPanel TermLibraryListPanel= createTermLibraryListPanel();
+        //创建翻译记忆库列表
+        JPanel MemoryLibraryListPanel=createMemoryLibraryListPanel();
 
         //创建主界面切换栏
         JPanel InterfaceSwitchingPanel=createInterfaceSwitchingPanel();
 
-        //创建术语库条目栏
-        JPanel TermLibraryPanel = createTermLibraryPanel();
+        //创建记忆库条目栏
+        JPanel MemoryLibraryItemsPanel = createMemoryLibraryItemsPanel();
 
         //创建条目筛选栏
         JPanel ItemSelectionPanel = createItemSelectionPanel();
@@ -44,14 +44,14 @@ public class TermLibraryInterface extends JFrame{
         ContentPanel.setLayout(null);
 
         setJMenuBar(menuBar);
-        ContentPanel.add(TermLibraryListPanel);
+        ContentPanel.add(MemoryLibraryListPanel);
         ContentPanel.add(InterfaceSwitchingPanel);
-        ContentPanel.add(TermLibraryPanel);
+        ContentPanel.add(MemoryLibraryItemsPanel);
         ContentPanel.add(ItemSelectionPanel);
 
-        TermLibraryListPanel.setBounds(0,0,200,400);
+        MemoryLibraryListPanel.setBounds(0,0,200,400);
         InterfaceSwitchingPanel.setBounds(0,400,200,250);
-        TermLibraryPanel.setBounds(200,100,800,500);
+        MemoryLibraryItemsPanel.setBounds(200,100,800,500);
         ItemSelectionPanel.setBounds(200,0,800,150);
 
         setContentPane(ContentPanel);
@@ -89,13 +89,13 @@ public class TermLibraryInterface extends JFrame{
         return menuBar;
     }
 
-    public JPanel createTermLibraryListPanel() {
+    public JPanel createMemoryLibraryListPanel() {
         JPanel panel1=new JPanel();
         panel1.setLayout(null);
 
         panel1.setBackground(Color.yellow);
 
-        JLabel label=new JLabel("创建术语库列表");
+        JLabel label=new JLabel("创建记忆库列表");
         label.setBounds(25,50,100,50);
         panel1.add(label);
 
@@ -209,13 +209,13 @@ public class TermLibraryInterface extends JFrame{
         return panel2;
     }
 
-    public JPanel createTermLibraryPanel(){
+    public JPanel createMemoryLibraryItemsPanel(){
         JPanel panel = new JPanel();
         panel.setLayout(null);
         Color Green = new Color(204,255,128);
         panel.setBackground(Green);
 
-        JLabel label = new JLabel("术语库条目");
+        JLabel label = new JLabel("记忆库条目");
         label.setFont(new Font("思源黑体", Font.BOLD, 25));
         Color Blue = new Color(64,0,128);
         label.setForeground(Blue);
@@ -227,11 +227,9 @@ public class TermLibraryInterface extends JFrame{
         table.setGridColor(new Color(192,192,192));
         table.setBorder(BorderFactory.createLineBorder(Color.BLUE));
 
-        JButton btn1 = new JButton("修改");
-        JButton btn4 = new JButton("删除");
+        JButton btn1 = new JButton("标记删除");
         JButton btn2 = new JButton("取消更改");
         JButton btn3 = new JButton("保存更改");
-
         JButton formerPage = new JButton("上一页");
         JButton nextPage = new JButton("下一页");
 
@@ -241,7 +239,6 @@ public class TermLibraryInterface extends JFrame{
         panel.add(btn1);
         panel.add(btn2);
         panel.add(btn3);
-        panel.add(btn4);
         panel.add(formerPage);
         panel.add(nextPage);
 
@@ -250,10 +247,9 @@ public class TermLibraryInterface extends JFrame{
         table.setBounds(30,50,740,378);
         formerPage.setBounds(570,450,80,20);
         nextPage.setBounds(690,450,80,20);
-        btn1.setBounds(90,450,90,20);
+        btn1.setBounds(200,450,90,20);
         btn2.setBounds(310,450,90,20);
         btn3.setBounds(420,450,90,20);
-        btn4.setBounds(200,450,90,20);
 
         return panel;
     }
@@ -269,18 +265,33 @@ public class TermLibraryInterface extends JFrame{
         Color Blue = new Color(64,0,128);
         label.setForeground(Blue);
 
+        JLabel origin = new JLabel("原文文本");
+        origin.setForeground(Color.BLACK);
+        origin.setFont(new Font("思源黑体", Font.BOLD, 20));
+        JLabel translation = new JLabel("译文文本");
+        translation.setForeground(Color.BLACK);
+        translation.setFont(new Font("思源黑体", Font.BOLD, 20));
+
         JTextField field1 = new JTextField(8);
         field1.setFont(new Font(null,Font.PLAIN,20));
+        JTextField field2 = new JTextField(8);
+        field2.setFont(new Font(null,Font.PLAIN,20));
 
         JButton selectButton = new JButton("筛选");
 
         panel.add(label);
+        panel.add(origin);
+        panel.add(translation);
         panel.add(field1);
+        panel.add(field2);
         panel.add(selectButton);
 
         label.setBounds(10,0,200,30);
-        field1.setBounds(140,40,300,30);
-        selectButton.setBounds(500,40,100,30);
+        origin.setBounds(70,30,100,32);
+        field1.setBounds(70,65,250,25);
+        translation.setBounds(360,30,100,32);
+        field2.setBounds(360,65,250,25);
+        selectButton.setBounds(650,40,100,30);
 
         return panel;
     }
