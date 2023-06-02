b7c6398de5de65b44fbd932d7d427a294399aa12
sing-ban <3073702249@qq.com>
2023-06-02 21:50:59 +0800

    singban-提交代码 项目创建没传上再补一遍

--- CAT_Desktop_System/src/UI_System/CreateProject/UI_FileListPanel.java
+++ CAT_Desktop_System/src/UI_System/CreateProject/UI_FileListPanel.java
@@ -21,6 +21,7 @@ public class UI_FileListPanel extends JPanel {
     public JButton deleteFileButton = new JButton();
 
     public List<String> fileNames = new ArrayList<>();
+    public List<String> filePaths=new ArrayList<>();
     public JTable fileTable = new JTable(4, 1);
     public JButton previousPage = new JButton();
     public JTextField pageNumber = new JTextField();
@@ -66,6 +67,17 @@ public class UI_FileListPanel extends JPanel {
         importFileButton.setMargin(new Insets(0, 0, 0, 0));
         deleteFileButton.setText("删除文件");
         deleteFileButton.setMargin(new Insets(0, 0, 0, 0));
+        deleteFileButton.addActionListener(new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int a=fileTable.getSelectedRow();
+                Object selectedRow=fileTable.getValueAt(a,0);
+                OnDeleteButtonDown((String)selectedRow);
+            }
+        });
+
+
+
         previousPage.setText("上一页");
         previousPage.setFont(new Font(null, Font.PLAIN, 10));
         previousPage.setMargin(new Insets(0, 0, 0, 0));
@@ -88,7 +100,7 @@ public class UI_FileListPanel extends JPanel {
         deleteFileButton.setBounds(130, 260, 60, 20);
         fileTable.setBounds(60, 300, 180, 80);
         //设置table的内容不可编辑
-        fileTable.setEnabled(false);
+       // fileTable.setEnabled(false);
 
         previousPage.setBounds(80, 400, 40, 20);
         nextPage.setBounds(170, 400, 40, 20);
@@ -128,9 +140,9 @@ public class UI_FileListPanel extends JPanel {
                 JFileChooser fileChooser = new JFileChooser();
                 int option = fileChooser.showOpenDialog(new UI_FileListPanel());
                 if (option == JFileChooser.APPROVE_OPTION) {
-                    fileNames.add(fileChooser.getSelectedFile().getAbsolutePath());
-                    System.out.println(fileNames.get(0));
-                    UpdateSavePathButton(fileNames);
+                    filePaths.add(fileChooser.getSelectedFile().getAbsolutePath());
+                    System.out.println(filePaths.get(0));
+                    UpdateSavePathButton(filePaths);
                 }
             }
         });
@@ -192,11 +204,24 @@ public class UI_FileListPanel extends JPanel {
     }
 
     private void UpdateTable(List<String> fileNames) {
-        for (int i = 0; i < fileNames.size(); i++)
+        for (int i = 0; i <4; i++)
+            fileTable.setValueAt("", i, 0);
+
+        for (int i = 0; i <fileNames.size(); i++)
             fileTable.setValueAt(fileNames.get(i), i, 0);
     }
 
     private void UpdateSavePathButton(List<String> fileNames) {
-        savePathField.setText(fileNames.get(fileNames.size() - 1));
+        savePathField.setText(filePaths.get(filePaths.size() - 1));
     }
+
+    private void OnDeleteButtonDown(String selectedString)
+    {
+        fileNames.removeIf(s->s.equals(selectedString));
+        UpdateTable(fileNames);
+//        for (int i = 0; i <fileNames.size(); i++)
+//            System.out.println(fileNames.get(i));;
+    }
+
+
 }
