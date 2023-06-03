86c30825eecac5e5c7c3c0d688b30ee79ca66e17
sing-ban <3073702249@qq.com>
2023-06-03 16:33:36 +0800

    singban-提交代码 术语添加

---
+++ CAT_Desktop_System/src/UI_System/TermAddWin/UI_ContentPanel.java
@@ -0,0 +1,44 @@
+package UI_System.TermAddWin;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class UI_ContentPanel extends JPanel {
+    public TextField text1;
+    public TextField text2;
+    public JLabel languageLable;
+    public JComboBox comboBox;
+    public JButton confirm;
+
+    public UI_ContentPanel()
+    {
+        this.setLayout(null);
+
+        text1=new TextField();
+        text2=new TextField();
+        languageLable=new JLabel("语言");
+        // String[] languages=new String[]{"Default","Chinese","English"};
+        comboBox=new JComboBox<>();
+        confirm=new JButton("确认");
+        confirm.setMargin(new Insets(0,0,0,0));
+
+
+        this.add(text1);
+        this.add(text2);
+        this.add(languageLable);
+        this.add(comboBox);
+        this.add(confirm);
+
+
+        text1.setBounds(25,50,150,25);
+        text2.setBounds(25,100,150,25);
+        languageLable.setBounds(200,50,25,25);
+        comboBox.setBounds(225,50,75,25);
+        confirm.setBounds(140,150,40,25);
+
+    }
+
+
+
+
+}
---
+++ CAT_Desktop_System/src/UI_System/TermAddWindow.java
@@ -0,0 +1,37 @@
+package UI_System;
+
+import javax.swing.*;
+import java.awt.*;
+import UI_System.TermAddWin.UI_ContentPanel;
+
+public class TermAddWindow extends JFrame {
+    public UI_ContentPanel contentPanel;
+
+    public TermAddWindow()
+    {
+        init();
+    }
+
+    public void init()
+    {
+        //窗口设置
+        setSize(325, 250);
+        setTitle("术语添加");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBackground(Color.decode("#EAECF2"));
+        setResizable(false);
+
+        contentPanel= new UI_ContentPanel();
+
+
+
+        setContentPane(contentPanel);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        TermAddWindow me=new TermAddWindow();
+    }
+}
+
