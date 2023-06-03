6c5301a9aeb44033c58e4463105c5a86e7718393
Maige798 <3317991292@qq.com>
2023-06-04 00:48:13 +0800

    Maige-修改代码

--- CAT_Desktop_System/src/UI_System/TermLibraryInputInterface.java
+++ CAT_Desktop_System/src/UI_System/TermLibraryInputInterface.java
@@ -8,40 +8,27 @@ public class TermLibraryInputInterface extends JFrame {
     //创建。。。宽200，高50，x:100,y:50(一个导航栏高）
     //表格：单元格宽300，高25
     //按钮：宽50，高25，x：100+200，y:50+50+25*3+20 ，x:100+300+50,y:（中间宽度两个单元格高）
-    public TermLibraryInputInterface()
-    {init();}
+    public TermLibraryInputInterface() {
+        init();
+    }
 
-    public void init()
-    {
+    public void init() {
         //窗口设置
-        setSize(800,500);
+        setSize(800, 500);
         setTitle("术语库导入");
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         //setBackground(Color.decode("#EAECF2"));
         setResizable(false);
 
-
-
-
         //创建内容面板
-
         JPanel ContentPanel = new UI_ContentPanel();
-
 
         setContentPane(ContentPanel);
         setLocationRelativeTo(null);
         setVisible(true);
-
-
-
     }
 
-
-
-
-
-
     public static void main(String[] args) {
-        TermLibraryInputInterface me=new TermLibraryInputInterface();
+        TermLibraryInputInterface me = new TermLibraryInputInterface();
     }
 }
