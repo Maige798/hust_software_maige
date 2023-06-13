47dadb6f1279e8c7a6aeca6a2dcc59dfa83ab632
lcwzy <273896827@qq.com>
2023-06-13 08:30:56 +0800

    lcwzy-提交代码 术语库创建和导入

---
+++ CAT_Desktop_System/src/UI_System/MachineTranslationWindow.java
@@ -0,0 +1,11 @@
+package UI_System;
+
+import javax.swing.*;
+
+public class MachineTranslationWindow extends JFrame {
+    public final String message= "";
+    public JLabel label1;
+    public JLabel label2;
+    public JLabel label3;
+    public JPanel panel;
+}
